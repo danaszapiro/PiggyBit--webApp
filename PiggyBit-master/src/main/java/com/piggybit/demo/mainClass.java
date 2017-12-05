8a8c5eae158970dfd6a0f16a9cf4421bfb7aefc4
Dana Szapiro <danasz@bu.edu>
2017-12-04 21:16:45 -0500

    Update mainClass.java

--- PiggyBit-master/src/main/java/com/piggybit/demo/mainClass.java
+++ PiggyBit-master/src/main/java/com/piggybit/demo/mainClass.java
@@ -1,13 +1,29 @@
 package com.piggybit.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class mainClass {
+public class mainClass implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(mainClass.class, args);
+	}
+	
+	@Override
+	public void run(String... strings) throws Exception {
+
+		User user1 = new User("DanaSzapiro", "password", "Dana", "Szapiro", "danasz@bu.edu");
+		User user2 = new User("IvanWong", "Password2", "Ivan", "Wong", "Ivan@bu.edu");
+
+		List<User> users = Arrays.asList(user1, user2);
+		userRepository.deleteAll();
+		userRepository.save(users);
 	}
 	/*
 	 * public static void main(String[] args) throws IOException, ParseException {
